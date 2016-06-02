ad4120eb2f86c2a4c2d5381f476cc1e1abdefd00
Klas Persson <resandeklas@hotmail.com>
2016-06-02 20:57:33 +0200

    Efter ES5 kata

--- JavaScript/spec/calculatorSpec.js
+++ JavaScript/spec/calculatorSpec.js
@@ -8,8 +8,20 @@ But also to do some TDD!
 
 */
 
+function verify(s,i) {
+     it("should evaluate '" + s + "' to " + i,function(){
+        expect(calculator.add(s)).toEqual(i);
+    });
+}
+
 describe("calculator",function(){
-    it("should evaluate \"\" to 0",function(){
-        expect(calculator.add("")).toEqual(0);
+    verify("",0);
+    verify("7",7);
+    verify("7,4",11);
+    verify("7,4,3,6",20);
+    verify("7,4,3,\n6", 20);
+    verify("//.\n1.5.7.8", 21);
+    it("-2,4,6,-5", function(){
+        expect(function(){calculator.add("-2,4,6,-5")}).toThrow(new Error("-2 -5"));
     });
 });
--- JavaScript/src/calculator.js
+++ JavaScript/src/calculator.js
@@ -1,5 +1,43 @@
 var calculator = {
-  add : function(expression) {
-    return parseInt(expression);
+  add : function(expression) { 
+   
+    if (expression === "") {
+      return 0;
+    }
+      
+   
+        
+    if (expression.indexOf("//") === 0) {
+      var parseoutdelimter = expression[2];
+      var row2 = expression.substr(3);
+      return foo(parseoutdelimter, row2)
+    }
+    else
+    {
+      return foo(",",expression)
+    }
   }
 }
+
+function foo(delimiter, s) {
+      var arr = s.split([delimiter]);
+    
+      if(arr.length === 1)
+         return parseInt(arr[0]);
+  
+      var negatives = [];    
+      var sum = 0;
+      arr.forEach(function(element) {
+         var i = parseInt(element);
+         if (i < 0)
+               negatives.push(i);
+         sum += i;
+        }, this);
+        
+       if (negatives.length > 0)
+          throw Error(negatives.join(" ")); 
+        
+      return sum;
+    }
+    
+   
